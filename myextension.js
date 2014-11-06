29ae0c51a31743b8c0d6fae2f293af84c1812cf1
Abdelfattah CHEHAB <achehab@MacBook-Pro-de-Abdelfattah.local>
2014-11-07 00:58:33 +0100

    sec

--- myextension.js
+++ myextension.js
@@ -12,7 +12,7 @@ var runCode = function() {
                      console.log("id:", id, "url:", url, 'body', body, 'xhr', xhr);
                      console.log(gmail.get.email_data(id));
                      var mymail = gmail.get.email_data(id).people_involved[0][1];
-                     console.log('new email!', mymail);
+                     console.log('new email!' + mymail);
                      window.postMessage({ "type": "new_email", "message_id": mymail }, "*");
                      });
     
