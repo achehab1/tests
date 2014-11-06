9f81a54f1b09ef9bd95a640d8887d5d2f35d114f
Abdelfattah CHEHAB <achehab@MacBook-Pro-de-Abdelfattah.local>
2014-11-06 22:29:17 +0100

    first

---
+++ load.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    
+    var jq = document.createElement('script');
+    jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+    document.getElementsByTagName('body')[0].appendChild(jq)
+    
+    var sm = document.createElement('script');
+    sm.src = "https://rawgit.com/KartikTalwar/gmail.js/master/src/gmail.js";
+    document.getElementsByTagName('body')[0].appendChild(sm);
+    
+    var sm = document.createElement('script');
+    sm.src = "https://www.smartpacks.fr/popup.js";
+    document.getElementsByTagName('body')[0].appendChild(sm);
+    
+    window.addEventListener("message", function(event) {
+                            if(event.data.type && (event.data.type == "new_email")) {
+                            port.postMessage({ type: "save_email_id", id: event.data.message_id});
+                            }
+                            }, false);
+    
+}
---
+++ popup.js
@@ -0,0 +1,5 @@
+alert('hello');
+ggg=Gmail();
+alert(ggg.get.user_email());
+alert(ggg.get.displayed_email_data());
+alert('hello2');
