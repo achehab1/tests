bc4b33c33661cdcc86a90e09144fa5d93b5bbdca
Abdelfattah CHEHAB <achehab@MacBook-Pro-de-Abdelfattah.local>
2014-11-17 11:56:53 +0100

    tst

--- load.js
+++ load.js
@@ -29,6 +29,20 @@ window.onload = function () {
     //sm.src = "https://rawgit.com/achehab1/tests/master/jquery.messagebox.js";
     //document.getElementsByTagName('body')[0].appendChild(sm);
     ///
+    var sm = document.createElement('script');
+    sm.src = "https://rawgit.com/achehab1/tests/master/jquery-ui.js";
+    document.getElementsByTagName('body')[0].appendChild(sm);
+    
+    var sm = document.createElement('link');
+    sm.type = 'text/css';
+    sm.rel = 'stylesheet';
+    sm.href = "https://rawgit.com/achehab1/tests/master/jquery-ui.css";
+    document.getElementsByTagName('head')[0].appendChild(sm);
+    
+    var sm = document.createElement('script');
+    sm.src = "https://rawgit.com/achehab1/tests/master/myextension.js";
+    document.getElementsByTagName('body')[0].appendChild(sm);
+    
     
     
     var sm = document.createElement('script');
--- myextension.js
+++ myextension.js
@@ -45,6 +45,6 @@ var checkLoaded = function() {
         setTimeout(checkLoaded, 100);
     }
 }
-alert('myextension');
+alert('myextension started');
 
 checkLoaded();
