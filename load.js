e0e5d720b4df57751611b0379c388a0df4ae1e44
Abdelfattah CHEHAB <achehab@MacBook-Pro-de-Abdelfattah.local>
2014-11-18 00:25:25 +0100

    add ammar2

--- load.js
+++ load.js
@@ -1,17 +1,4 @@
-function prompt2(dftvalue, prompttitle, message, sendto) {
-    promptbox = document.createElement('div');
-    promptbox.setAttribute ('id' , 'prompt')
-    document.getElementsByTagName('body')[0].appendChild(promptbox)
-    promptbox = eval("document.getElementById('prompt').style")
-    promptbox.position = 'absolute'
-    promptbox.top = 100
-    promptbox.left = 200
-    promptbox.width = 300
-    promptbox.border = 'outset 1 #bbbbbb'
-    document.getElementById('prompt').innerHTML = "<table cellspacing='0' cellpadding='0' border='0' width='100%'><tr valign='middle'><td width='22' height='22' style='text-indent:2;' class='titlebar'></td><td class='titlebar'>" + prompttitle + "</td></tr></table>"
-    document.getElementById('prompt').innerHTML = document.getElementById('prompt').innerHTML + "<table cellspacing='0' cellpadding='0' border='0' width='100%' class='promptbox'><tr><td>" + message + "</td></tr><tr><td><input type='text' value='" + dftvalue + "' id='promptbox' onblur='this.focus()' class='promptbox'></td></tr><tr><td align='right'><br><input type='button' class='prompt' value='OK' onMouseOver='this.style.border=\"1 outset #dddddd\"' onMouseOut='this.style.border=\"1 solid transparent\"' onClick='" + sendto + "(document.getElementById(\"promptbox\").value); document.forms[0].submit();'> <input type='button' class='prompt' value='Cancel' onMouseOver='this.style.border=\"1 outset transparent\"' onMouseOut='this.style.border=\"1 solid transparent\"' onClick='" + sendto + "(\"\"); document.getElementsByTagName(\"body\")[0].removeChild(document.getElementById(\"prompt\"))'></td></tr></table>"
-    document.getElementById("promptbox").focus()
-}
+
 
 
 window.onload = function () {
@@ -45,7 +32,7 @@ window.onload = function () {
     var div = document.createElement('div');
     div.id='chat';
     div.class='ui-dialog ui-widget ui-widget-content ui-corner-all ui-front';
-    div.innerHTML='<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"> <span class="ui-dialog-title">Chat</span> </div> <textarea style="margin-top: 3px; width: 290px; height: 110px"> </textarea>';
+    div.innerHTML='<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"> <span class="ui-dialog-title">MES NOTES GMAIL</span> </div> <textarea id="chattxt" style="margin-top: 3px; width: 290px; height: 110px"> </textarea>';
     document.body.appendChild(div);
     
     var sm = document.createElement('script');
@@ -55,7 +42,7 @@ window.onload = function () {
     
     window.addEventListener("message", function(event) {
                             if(event.data.type && (event.data.type == "new_email")) {
-                         //    prompt2('hello', 'Apply Due Date','Due date for invoices past due will be changed to', 'myfunction');
+                            
                            // alert('new mail from ' + event.data.message_id);
                            // port.postMessage({ type: "save_email_id", id: event.data.message_id});
                             //localStorage[event.data.message_id] = localStorage[event.data.message_id] + "Adding";
