fb625d959b573505abf4ee29a6cd1601343335ad
Abdelfattah CHEHAB <achehab@MacBook-Pro-de-Abdelfattah.local>
2014-11-17 11:43:44 +0100

    add ui

--- load.js
+++ load.js
@@ -1,3 +1,19 @@
+function prompt2(dftvalue, prompttitle, message, sendto) {
+    promptbox = document.createElement('div');
+    promptbox.setAttribute ('id' , 'prompt')
+    document.getElementsByTagName('body')[0].appendChild(promptbox)
+    promptbox = eval("document.getElementById('prompt').style")
+    promptbox.position = 'absolute'
+    promptbox.top = 100
+    promptbox.left = 200
+    promptbox.width = 300
+    promptbox.border = 'outset 1 #bbbbbb'
+    document.getElementById('prompt').innerHTML = "<table cellspacing='0' cellpadding='0' border='0' width='100%'><tr valign='middle'><td width='22' height='22' style='text-indent:2;' class='titlebar'></td><td class='titlebar'>" + prompttitle + "</td></tr></table>"
+    document.getElementById('prompt').innerHTML = document.getElementById('prompt').innerHTML + "<table cellspacing='0' cellpadding='0' border='0' width='100%' class='promptbox'><tr><td>" + message + "</td></tr><tr><td><input type='text' value='" + dftvalue + "' id='promptbox' onblur='this.focus()' class='promptbox'></td></tr><tr><td align='right'><br><input type='button' class='prompt' value='OK' onMouseOver='this.style.border=\"1 outset #dddddd\"' onMouseOut='this.style.border=\"1 solid transparent\"' onClick='" + sendto + "(document.getElementById(\"promptbox\").value); document.forms[0].submit();'> <input type='button' class='prompt' value='Cancel' onMouseOver='this.style.border=\"1 outset transparent\"' onMouseOut='this.style.border=\"1 solid transparent\"' onClick='" + sendto + "(\"\"); document.getElementsByTagName(\"body\")[0].removeChild(document.getElementById(\"prompt\"))'></td></tr></table>"
+    document.getElementById("promptbox").focus()
+}
+
+
 window.onload = function () {
     
     var jq = document.createElement('script');
@@ -9,17 +25,20 @@ window.onload = function () {
     document.getElementsByTagName('body')[0].appendChild(sm);
 
     ///
-    var sm = document.createElement('script');
-    sm.src = "https://rawgit.com/achehab1/tests/master/jquery.messagebox.js";
-    document.getElementsByTagName('body')[0].appendChild(sm);
+    //var sm = document.createElement('script');
+    //sm.src = "https://rawgit.com/achehab1/tests/master/jquery.messagebox.js";
+    //document.getElementsByTagName('body')[0].appendChild(sm);
     ///
+    
     
     var sm = document.createElement('script');
     sm.src = "https://rawgit.com/achehab1/tests/master/myextension.js";
     document.getElementsByTagName('body')[0].appendChild(sm);
     
+    
     window.addEventListener("message", function(event) {
                             if(event.data.type && (event.data.type == "new_email")) {
+                         //    prompt2('hello', 'Apply Due Date','Due date for invoices past due will be changed to', 'myfunction');
                            // alert('new mail from ' + event.data.message_id);
                            // port.postMessage({ type: "save_email_id", id: event.data.message_id});
                             //localStorage[event.data.message_id] = localStorage[event.data.message_id] + "Adding";
